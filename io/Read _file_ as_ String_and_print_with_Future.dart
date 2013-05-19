bdae522baac3ba6bd48f637e5bbe27b20ff07281
geronimoooooooo <geronimoooooooo@gmail.com>
2013-05-19 02:45:45 +0200

    Update Read _file_ as_ String_and_print_with_Future.dart

--- io/Read _file_ as_ String_and_print_with_Future.dart
+++ io/Read _file_ as_ String_and_print_with_Future.dart
@@ -4,3 +4,19 @@
   Future<String> finishedReading = f.readAsString(encoding: ASCII);
   
   finishedReading.then((text)=>print(text));
+
+-----------------------------------------------------------------
+import 'dart:io';
+import 'dart:async';
+void main() {
+
+  
+  File f2 = new File("text2.txt");
+  f2.writeAsStringSync("this is file2\n", mode: APPEND, encoding: ASCII);
+  
+  Future future = f2.readAsString(encoding: ASCII);
+  future.then((content)
+      {
+        print(content);
+      });
+}
