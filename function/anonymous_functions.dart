c64d415a1a676c019dcb4fac3638e071b95e9592
geronimoooooooo <geronimoooooooo@gmail.com>
2013-07-28 15:07:15 +0200

    Create anonymous_functions.dart

---
+++ function/anonymous_functions.dart
@@ -0,0 +1,11 @@
+Functions as first class objects. 
+* You can pass functions as an argument
+* You can save functions to variables
+* use for anonymous functions
+  
+  var myAnonymous= (e,f){
+   return"Hi $e $f";
+   };
+  
+  var x=myAnonymous("e","f");
+   print(x);
