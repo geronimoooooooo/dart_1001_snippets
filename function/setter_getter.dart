60f6591e89846733544af8bab0d6542cba44b5b7
geronimoooooooo <geronimoooooooo@gmail.com>
2013-07-31 20:07:45 +0200

    Create setter_getter.dart

---
+++ function/setter_getter.dart
@@ -0,0 +1,14 @@
+class Example21 {
+  List<String> _names;
+  Example21() {
+    _names = ["a", "b"]; 
+  }
+  List<String> get names => _names;
+  set names(List<String> list) {
+    _names = list;
+  }
+  int get length => _names.length;
+  void add(String name) {
+    _names.add(name);
+  }
+}
