fbdc0e400fd945f82893287f8b85ff3d057ce60f
geronimoooooooo <geronimoooooooo@gmail.com>
2015-04-01 11:34:00 +0200

    Delete future_wait_multiple_simultan.dart

--- Future/future_wait_multiple_simultan.dart
+++
@@ -1,6 +0,0 @@
-//http://www.dartlang.org/articles/using-future-based-apis/#calling-multiple-functions-that-return-futures
-
-Future.wait([expensiveA(), expensiveB(), expensiveC()])
-      .then((List responses) => chooseBestResponse(responses))
-      .catchError((e) => handleError(e));
-    
